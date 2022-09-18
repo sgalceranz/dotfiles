5210df6b1245eedb089698ffe210d82e5d3aee7d
Sergio Galceran <sgalceranz@gmail.com>
2022-09-18 12:53:26 -0600

    chore: update Powershell profile

--- home/Documents/PowerShell/Profile.ps1
+++
@@ -1,6 +0,0 @@
-. $HOME\Documents\WindowsPowerShell\Profile.ps1
-
-function Edit-ProfilePS7 {
-    & code --reuse-window $HOME\Documents\PowerShell\Profile.ps1
-}
-Set-Alias "ep7" -Value "Edit-ProfilePS7"
